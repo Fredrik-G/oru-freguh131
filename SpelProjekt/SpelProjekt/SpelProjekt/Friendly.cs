6e983e1486f8d1a51b970ac58ddb7555353bbe64
gamwed@gmail.com <gamwed@gmail.com@b5e28ae6-2070-c97b-bfc2-c1ff929b6310>
2014-11-09 16:45:29 +0000

    Lade till lite mer kommentarer för doxygen.

--- SpelProjekt/SpelProjekt/SpelProjekt/Friendly.cs
+++ SpelProjekt/SpelProjekt/SpelProjekt/Friendly.cs
@@ -15,6 +15,7 @@ namespace SpelProjekt
     class Friendly
     {
         Vector2 position;
+        public Vector2 Position { get { return position; } }  
         Texture2D picture;
         Texture2D madPicture;
         float speed;
@@ -31,7 +32,14 @@ namespace SpelProjekt
             set { mad = value; }
             get { return mad; }
         }
-
+        /// <summary>
+        /// Konstruktor för friendly.
+        /// </summary>
+        /// <param name="picture">Textur som ska visas för friendly</param>
+        /// <param name="startPosition">Startposition</param>
+        /// <param name="speed">Hastighet</param>
+        /// <param name="deltaX"> </param>
+        /// <param name="madPicture">Default parameter som används med fågeln Roger för att gå åt andra hållet.</param>
         public Friendly(Texture2D picture, Vector2 startPosition, float speed, float deltaX, Texture2D madPicture = null)
         {
             this.picture = picture;
@@ -39,9 +47,11 @@ namespace SpelProjekt
             this.speed = speed;
             this.deltaX = deltaX;
             this.madPicture = madPicture;
-        }
-        public Vector2 Position { get { return position; }
-        }
+        }     
+        /// <summary>
+        /// Visa friendly.
+        /// </summary>
+        /// <param name="batch"></param>
         public void Draw(SpriteBatch batch)
         {
             if (mad == 0)
@@ -53,6 +63,10 @@ namespace SpelProjekt
                     40.0f, 20.0f), 1.0f, SpriteEffects.None, 0.0f);
             }
         }
+        /// <summary>
+        /// Uppdaterar friendlyns position.
+        /// </summary>
+        /// <param name="gameTime"></param>
         public void Update(GameTime gameTime)
         {
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -63,8 +77,14 @@ namespace SpelProjekt
                 position.X += speed * elapsed;
             position.Y += deltaX * elapsed;
         }
+        /// <summary>
+        /// Kollar om en av spelarens skott träffade en motståndare.
+        /// </summary>
+        /// <param name="bulletList">Tar emot en lista innehållande alla skott.</param>
+        /// <returns>Returnerar skottets position i listan om ett skott träffade en friendly.
+        /// Om inget skott träffade så returneras -1.</returns>
         public int CollisionBullet(List<Bullet> bulletList)
-        {//Kollar om en av spelarens skott träffade en motståndare.
+        {
             for (int i = 0; i < bulletList.Count; i++)
             {
                 if ((bulletList[i].Position - position).Length() < radius)
@@ -72,15 +92,15 @@ namespace SpelProjekt
             }
             return -1;
         }
+        /// <summary>
+        /// Används för att flytta fågeln Roger åt höger istället för vänster.
+        /// </summary>
+        /// <param name="gameTime"></param>
         public void MoveBack(GameTime gameTime)
         {
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
             //Flyttar åt höger
             position.X += speed * elapsed;
-        }
-        public void Destroy()
-        {
-
         }
     }
 }

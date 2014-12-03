b48e02e5aa731d4a499ee8af4e102af9f58ec42c
gamwed@gmail.com <gamwed@gmail.com@b5e28ae6-2070-c97b-bfc2-c1ff929b6310>
2014-12-03 18:00:32 +0000

    h-lkfxzÖH>S

--- trunk/AI Course/PythonApplication1/node.py
+++ trunk/AI Course/PythonApplication1/node.py
@@ -2,8 +2,10 @@ class Node(object):
     name = ""
     x = 0
     y = 0
-    cost = 0
-    
+    cost_so_far = 0.0
+    cost_to_goal = 0.0
+    total = 0.0
+    parent = []
 
     def __init__ (self,namn,X,Y):
         self.name = namn

9698da38701a4e10f96dab87e00e4e4d94ce10ca
gamwed <gamwed@b5e28ae6-2070-c97b-bfc2-c1ff929b6310>
2014-12-18 16:30:21 +0000

    AI uppgift 2 klar

--- AI Course/labb2/labb2.py
+++ AI Course/labb2/labb2.py
@@ -2,7 +2,7 @@ from room import *
 from person import *
 
 personList = [person("-1", "asd", False, -1) for i in range(9)]
-roomList = [room("-1", -1, -1) for i in range(7)]
+roomList = [room("-1", -1, -1, 0) for i in range(7)]
 def initLists():
     personList[0].id = 'A'
     personList[0].profession = 'Boss'
@@ -42,7 +42,7 @@ def initLists():
     personList[7].id = 'H'
     personList[7].profession = 'Worker'
     personList[7].garlic_lover = True
-    personList[7].max_visitors = 7
+    personList[7].max_visitors = 0
 
     personList[8].id = 'I'
     personList[8].profession = 'Worker'
@@ -102,71 +102,360 @@ def initLists():
     roomList[5].near.append(roomList[6])
     roomList[5].near.append(roomList[1])
 
-    roomList[2].near.append(roomList[5])
-    roomList[2].near.append(roomList[0])
+    roomList[6].near.append(roomList[0])
+    roomList[6].near.append(roomList[5])
 
-
-def AssignPersonToRoom():
-    i = 0
-    for person in personList:
-        if(i>5 and i<10):
-            person.inroom = roomList[i-5]
-            roomList[i-5].workersInRoom.append(person)
-        else:
-            person.inroom = roomList[i]
-            roomList[i].workersInRoom.append(person)
-
-        i += 1
-
-    print(1)
 
 def bossConstraints(person, room):
-    #A “boss” shall sit alone in an office
+    #A boss shall sit alone in an office
     if(person.profession == 'Boss'):
-        if(len(room.workersInRoom) < 1):
-            return True
+        if(len(room.workersInRoom) > 1):
+            return False
+
     else:
-        if(len(room.workersInRoom) != 0):
-            if(room.workersInRoom[-1].profession == 'Boss'):
+        for worker in room.workersInRoom:
+            if(worker.profession == 'Boss'):
                 return False
-            else:
+
+    return True
+
+def SecretaryConstraints(person, room):
+    #A secretary person shall sit in an office near to a ?boss? 
+    if(person.profession != 'Secretary'):
+        return True
+    for adjacent in room.near:
+        for worker in adjacent.workersInRoom:
+            if(worker.profession == 'Boss'):
                 return True
 
-def SecretaryAndITConstraints(person, room):
-    #An “it-support” person shall sit in an office near to a “boss” 
-    if(person.profession == 'Secretary'):
-        for adjacent in room.near:
-            for worker in adjacent.workersInRoom:
-                if(worker.profession == 'Boss'):
-                    return True
+        return False
 
-        return False    
+def ITConstraints(person, room):
+    #An ?it-support? person shall sit in an office near to a ?boss? 
+    if(person.profession != 'IT-support'):
+        return True
+    for adjacent in room.near:
+        for worker in adjacent.workersInRoom:
+            if(worker.profession == 'Boss'):
+                return True
+
+        return False
 
 def garlicLoverConstraints(person, room):
     #A garlic-loving person shall either sit alone or together with other garlic-lovers
-    number_of_workers = len(room.workersInRoom)
-    number_of_garlic_lovers = 0
-    if(number_of_workers > 0):
-        for worker in room.workersInRoom:
-            if(worker.garlic_lover == True):
-                number_of_garlic_lovers += 1
-        
-        if(number_of_garlic_lovers == number_of_workers):
-            return True
-        else:
-            return False        
+    for worker in room.workersInRoom:
+        if(person.garlic_lover != worker.garlic_lover):
+            return False
 
-    else:
-       return True
+    return True
+
 def visitorConstraints(person, room):
 #A person with non-zero visitors shall sit in an office that provides enough space for
-#the number of visitors – the maximum number of visitors of all persons in the room
-#shall be smaller or equal to the possible number of visitors.     if(room.visitor_places >= person.max_visitors):        sum_of_visitors = 0        for worker in room.workersInRoom:            if(person != worker):                sum_of_visitors += worker.max_visitors        if(room.visitor_places >= (person.max_visitors + sum_of_visitors)):            return True        else:            return False
+#the number of visitors ? the maximum number of visitors of all persons in the room
+#shall be smaller or equal to the possible number of visitors. 
+    
+    count=0
+    if(person.max_visitors > 0):
+        for worker in room.workersInRoom:
+            count = count + worker.max_visitors
+
+    count = count + person.max_visitors
+
+    if(count>room.visitor_places):
+        return False
+    else:
+        return True
+
+    #if(room.visitor_places >= person.max_visitors):
+    #    sum_of_visitors = 0
+    #    for worker in room.workersInRoom:
+    #        if(person != worker):
+    #            sum_of_visitors += worker.max_visitors
+
+    #    if(room.visitor_places >= (person.max_visitors + sum_of_visitors)):
+    #        return True
+    #    else:
+    #        return False
+
+def allocate(persons, rooms):
+    for person in persons:
+        for room in rooms:
+            if(AssignPersonToRoom(person, room)):
+                break
+
+def CheckConstraints(person, room):
+    if(bossConstraints(person, room) and 
+    SecretaryConstraints(person, room) and 
+    ITConstraints(person, room) and 
+    garlicLoverConstraints(person, room) and 
+    visitorConstraints(person, room)):
+        return True
+
+    return False
+#############################################################################################################
+
+def CheckBossAlone(person,room,personinroom):
+    if(person.profession == "Boss"):
+        for key, value in personinroom.items():
+            if(value == room):
+                return False
+
+    for key, value in personinroom.items():
+        if(value == room):
+            if(key.profession == "Boss"):
+                return False
+    return True
+
+def CheckSecretaryNearBoss(person,room,personinroom):
+    if(person.profession == "Secretary"):
+        for key,value in personinroom.items():
+            if(key.profession == "Boss"):
+                for x in value.near:
+                    if(x.id == room.id):
+                        return True
+        return False
+    else:
+        return True
+
+def CheckITSupportNearBoss(person,room,personinroom):
+    if(person.profession == "IT-support"):
+        for key,value in personinroom.items():
+            if(key.profession == "Boss"):
+                for x in value.near:
+                    if(x.id == room.id):
+                        return True
+        return False
+    else:
+        return True
+
+def CheckGarlicLover(person, room, personinroom):
+    if(person.garlic_lover == True):
+        for key,value in personinroom.items():
+            if(room == value):
+                if(key.garlic_lover == False):
+                    return False
+    else:
+        for key,value in personinroom.items():
+            if(room == value):
+                if(key.garlic_lover == True):
+                    return False
+    return True
+
+def CheckVisitorSpace(person, room, personinroom):
+    count = 0
+    if(person.max_visitors > 0):
+        for key, value in personinroom.items():
+            if(room == value):
+                count = count + key.max_visitors
+    count = count + person.max_visitors
+
+    if(count > room.visitor_places):
+        return False
+    else:
+        return True
+
+def CheckPlace(person,room,personinroom):
+    count = 0
+    for key, value in personinroom.items():
+        if(room == value):
+            count = count + 1
+    count = count + 1
+
+    if(count > room.work_places):
+        return False
+    else:
+        return True 
+
+def CheckAllConstraints(person,room,personinroom):
+    return (CheckBossAlone(person,room,personinroom) and
+            CheckSecretaryNearBoss(person,room,personinroom) and
+            CheckITSupportNearBoss(person,room,personinroom) and
+            CheckGarlicLover(person,room,personinroom) and
+            CheckVisitorSpace(person, room, personinroom) and
+            CheckPlace(person,room,personinroom))
+
+
+
+
+
+
+
+
+###################################################################################################################
+def AllocateBacktracking():
+    i = 0
+    #select unassigned variable(someone who's not in a room)
+    while(personList[i].inroom != None):
+        i += 1
+        if(i >= len(personList)):
+            #alla är i ett rum, return complete
+            return True
+
+    for room in roomList:
+        personList[i].inroom = room
+        room.workersInRoom.append(personList[i])
+
+        #kolla om personen kan vara i det rummet
+        #ta  bort om den inte kan vara där
+        if not CheckConstraints(personList[i], room):
+            personList[i].inroom = None
+            room.workersInRoom.remove(personList[i])
+
+        #fortsätt med nästa person
+        elif AllocateBacktracking():
+            return True
+
+        #ta bort denna persons rum om det blev ett fel längre fram
+        else:
+            personList[i].inroom = None
+            room.workersInRoom.remove(personList[i])
+
+    return False
+
+def NumberOfPossibleRooms(person, rooms):
+    numberOfRooms = 0
+    for room in rooms:
+        if(CheckConstraints(person, room)):
+            numberOfRooms += 1
+
+    return numberOfRooms
+
+
+#def GetSortedRoomList(person, rooms, unassignedPersons):
+#    eval = {}
+#    for room in rooms:
+#        if(CheckConstraints(person, room)):
+#            assign()
+
+#        blah=0
+#        for p in unassignedPersons:
+#            blah=blah+len(possibleRooms)
+        
+#        eval[r] = blah
+        
+#        return sort unassigned
+
+#def GetPossibleRoom(person, rooms):
+#    bla=[]
+#    for room in rooms:
+#        if(CheckConstraints(person, room)):
+#            bla.append(room)
+
+#    return bla
+
+
+PersonInRoom = {}
+
+
+def SelectUnassignedVariable(personlist,roomlist,personinroom):
+    #Ta fram person med flest constraints (kan vara i minst antal rum)
+    TempPersonList = []
+    if(len(personinroom) == 0):
+        for person in personlist:
+            if(person.profession == "Boss"):
+                return person
+
+    for person in personlist:
+        if person not in personinroom:
+            count = 0
+            for x in roomlist:
+                if(CheckConstraints(person,x)):
+                    count = count + 1
+
+            person.AvailableRooms = count
+            TempPersonList.append(person)
+
+    if(len(TempPersonList) != 0):
+        return min(TempPersonList, key = lambda x: x.AvailableRooms)
+    else:
+        return None
+def OrderDomainValues(person,roomlist,personinroom,personlist):
+    tempRoomList = []
+
+    for room in roomlist:
+        PossibleOptions = 0
+        #Lägg in personen man skickar med i ett rum
+        #Testa sedan (test-allocation) att lägga in resterande personer i ett rum
+        #och kolla hur många som "får" vara där.
+        personinroom[person] = room
+
+        for testperson in personlist:
+            if testperson not in personinroom:
+                count = 0
+                for room2 in roomlist:
+                    if(CheckAllConstraints(testperson, room2, personinroom)):
+                        count += 1
+
+                PossibleOptions += count
+
+        room.possibleOptions = PossibleOptions
+        del personinroom[person]
+
+    TempRoomList = roomlist
+    sortedRoomList = []
+
+    while(len(TempRoomList) != 0):
+        #Sorterar listan i storlektsordning med avseende på possibleOption
+        BestRoom = max(TempRoomList, key = lambda room: room.possibleOptions)
+        sortedRoomList.append(BestRoom)
+        TempRoomList.remove(BestRoom)
+
+    return sortedRoomList
+        
+def RecursiveAllocate(personlist,roomlist,personinroom):
+    #Smart tilldelning för att minska antal steg (backtracking).
+    #Utgå från person med flest constraints och rum med flest antal möjliga arbetare
+
+    #Ta fram person med flest constraints
+    person = SelectUnassignedVariable(personlist,roomlist,personinroom)
+    if(person == None):
+        return personinroom
+    #Ta fram en sorterad rumlista med rummet med flest antal möjliga arbetare först.
+    roomlist = OrderDomainValues(person,roomlist,personinroom,personlist)
+    for room in roomlist:
+        if(CheckConstraints(person,room) == True):
+            #Lägg in den "bästa" personen om den kan vara där
+           personinroom[person] = room
+           room.workersInRoom.append(person)
+           result = RecursiveAllocate(personlist,roomlist,personinroom)
+
+           if (result != False):
+               return result
+
+           del personinroom[person]
+           room.workersInRoom.remove(person)
+
+    return False
+
+def PrintDict(personinroom):
+    print("(ID,Profession,Garlic Lover,Max visitors,rum-ID)")
+    for key,value in personinroom.items():
+        print("(",key.id,",",key.profession,",",key.garlic_lover,",",key.max_visitors,")",":",value.id)
+        
+
 
 initLists()
-AssignPersonToRoom()
+#AssignPersonToRoom()
 
-SecretaryAndITConstraints(personList[1], roomList[1])
+#allocate(personList, roomList)
 
-print(garlicLoverConstraints(personList[3], roomList[1]))
-print(visitorConstraints(personList[8], roomList[5]))
+#print(AllocateBacktracking())
+
+#AllocateHeuristics()
+
+
+#PersonInRoom[personList[0]] = roomList[0]
+#asd=SelectUnassignedVariable(PersonInRoom)
+#OrderDomainValues(personList[0])
+
+
+PrintDict(RecursiveAllocate(personList, roomList, PersonInRoom))
+
+
+
+print('\n\n\n\n')
+
+#SecretaryAndITConstraints(personList[1], roomList[1])
+
+#print(garlicLoverConstraints(personList[3], roomList[1]))
+#print(visitorConstraints(personList[8], roomList[5]))
--- AI Course/labb2/room.py
+++ AI Course/labb2/room.py
@@ -4,10 +4,12 @@ class room(object):
     visitor_places = -1
     near = []
     workersInRoom = []
+    possibleOptions = 0
 
-    def __init__(self, id, work_places, visitor_places):
+    def __init__(self, id, work_places, visitor_places, possible):
         self.id = id
         self.work_places = work_places
         self.visitor_places = visitor_places
         self.near = []
         self.workersInRoom = []
+        self.possibleOptions = 0

b281274f335006082f18096fb26ea258558b7327
gamwed <gamwed@b5e28ae6-2070-c97b-bfc2-c1ff929b6310>
2014-12-18 16:30:21 +0000

    AI uppgift 2 klar

--- trunk/AI Course/labb2/person.py
+++ trunk/AI Course/labb2/person.py
@@ -4,6 +4,7 @@ class person(object):
     garlic_lover = False
     max_visitors = -1
     inroom = None
+    AvailableRooms = 0
 
     def __init__(self, id, profession, garlic_lover, max_visitors):
         self.id = id
@@ -11,3 +12,4 @@ class person(object):
         self.garlic_lover = garlic_lover
         self.max_visitors = max_visitors
         inroom = None
+        AvailableRooms = 0
